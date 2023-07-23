5d64c8de315c0ac4427f0857f836a70e3f8eb914
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-23 21:49:51 +0530

    Update auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -1,65 +1,34 @@
-// authentication-service/index.js
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const port = 5001;
 const app = express();
 
-const privateKey = fs.readFileSync('private_key.pem');
-const publicKey = fs.readFileSync('public_key.pem');
+const secretKey = process.env.SECRET_KEY; // Replace this with your actual secret key
 
 app.use(cors());
 app.use(bodyParser.json());
 
-function generateToken(payload) {
-  return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-}
-
-function verifyToken(token) {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, publicKey, { algorithms: ['RS256'] }, (err, decoded) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(decoded);
-      }
-    });
-  });
-}
-
+// Endpoint for generating a token
 app.post('/generate-token', (req, res) => {
-  const { userId } = req.body;
-  if (!userId) {
-    return res.status(400).json({ error: 'User ID not provided' });
+  const { publicKey } = req.body;
+  console.log('Received publicKey:', publicKey); // Add this log to see the incoming data
+
+  if (!publicKey) {
+    return res.status(400).json({ error: 'Public key not provided' });
   }
 
   const payload = {
-    sub: userId,
+    sub: publicKey,
   };
 
-  const token = generateToken(payload);
+  const token = jwt.sign(payload, secretKey, { algorithm: 'HS256' });
+  console.log('Generated token:', token); // Add this log to see the generated token
   res.json({ token });
 });
 
-app.post('/verify-token', (req, res) => {
-  const { token } = req.body;
-  if (!token) {
-    return res.status(400).json({ error: 'Token not provided' });
-  }
-
-  verifyToken(token)
-    .then((decoded) => {
-      res.json({ valid: true, decoded });
-    })
-    .catch((err) => {
-      console.error('Token verification failed:', err);
-      res.json({ valid: false, error: err.message });
-    });
-});
-
-const port = 5001;
 app.listen(port, () => {
   console.log(`Authentication service is running on http://localhost:${port}`);
 });
+
