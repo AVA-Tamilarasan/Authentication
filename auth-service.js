e4c9eb7bb4ea5706eb3932f03369b61da401a5f1
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-23 14:55:37 +0530

    Update auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const port = 5001;
 const app = express();
 
+const secretKey = 'my-private-key'; // Replace this with your actual secret key
+
 const privateKey = `-----BEGIN PRIVATE KEY-----
 MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCTkx5o7cEBYySe
 nGK8p8Lm2L6hqbA2LCmWQp1HwVcm/4xzFIuovRmNmVWBUhc2BJvAXHVaEZjWJ2eh
@@ -44,27 +46,22 @@ qMjrzZlYu6uCHc2NDNVp27hrwTLaAnQ50SShCjOjmn7kdKfZ6fEC9IAHt8h7BMbc
 nwIDAQAB
 -----END PUBLIC KEY-----`;
 
-app.use(cors({ origin: process.env.REACT_APP_BACKEND_URL }));
+
+app.use(cors());
 app.use(bodyParser.json());
 
 // Endpoint for generating a token
 app.post('/generate-token', (req, res) => {
   const { publicKey } = req.body;
-  console.log('Received publicKey:', publicKey);
+  console.log('Received publicKey:', publicKey); // Add this log to see the incoming data
 
   if (!publicKey) {
     return res.status(400).json({ error: 'Public key not provided' });
   }
 
-  try {
-    // Sign the token using the private key and subject (sub) as the public key
-    const token = jwt.sign({}, privateKey, { algorithm: 'RS256', subject: publicKey });
-    console.log('Generated token:', token);
-    res.json({ token });
-  } catch (error) {
-    console.error('Error generating token:', error);
-    res.status(500).json({ error: 'Failed to generate token' });
-  }
+  const token = jwt.sign({}, secretKey, { subject: publicKey, algorithm: 'HS256' });
+  console.log('Generated token:', token); // Add this log to see the generated token
+  res.json({ token });
 });
 
 // Middleware to verify the token
@@ -77,16 +74,16 @@ function authenticateToken(req, res, next) {
 
   console.log('Received token:', token);
 
-  try {
-    // Verify the token using the public key
-    const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
+    if (err) {
+      console.error('Token verification failed:', err);
+      return res.sendStatus(403);
+    }
+
     req.user = decoded.sub;
     console.log('User authorized:', req.user);
     next();
-  } catch (err) {
-    console.error('Token verification failed:', err);
-    return res.sendStatus(403);
-  }
+  });
 }
 
 app.get('/backend', authenticateToken, (req, res) => {
