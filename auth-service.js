2d4a3968a4379a8f2db4a86224f29471b48528dd
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-23 12:33:30 +0530

    Update auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -14,8 +14,6 @@ app.use(bodyParser.json());
 // Endpoint for generating a token
 app.post('/generate-token', (req, res) => {
   const { publicKey } = req.body;
-  console.log('Received publicKey:', publicKey); // Add this log to see the incoming data
-
   if (!publicKey) {
     return res.status(400).json({ error: 'Public key not provided' });
   }
@@ -32,9 +30,7 @@ function authenticateToken(req, res, next) {
     return res.sendStatus(401);
   }
 
-  console.log('Received token:', token);
-
-  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
+  jwt.verify(token, secretKey, (err, decoded) => {
     if (err) {
       console.error('Token verification failed:', err);
       return res.sendStatus(403);
@@ -43,21 +39,8 @@ function authenticateToken(req, res, next) {
     req.user = decoded.sub;
     console.log('User authorized:', req.user);
     next();
-  });
+  }); // <--- Missing closing brace here
 }
-
-
-  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
-    if (err) {
-      console.error('Token verification failed:', err);
-      return res.sendStatus(403);
-    }
-
-    req.user = decoded.sub;
-    console.log('User authorized:', req.user);
-    next();
-  });
-
 
 app.get('/backend', authenticateToken, (req, res) => {
   res.json({ message: 'Authorized access to the backend!', user: req.user });
