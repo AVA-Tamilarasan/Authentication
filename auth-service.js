ffc9aebc9cc3b22d9492f9be354a20f0f1fec4b5
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-24 21:43:54 +0530

    Update auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const axios = require('axios');
 const port = 5001;
 const app = express();
 
@@ -11,10 +12,9 @@ const privateKey = fs.readFileSync('private_key.pem'); // Load the private key f
 app.use(cors());
 app.use(bodyParser.json());
 
-// Endpoint for generating a token
-app.post('/generate-token', (req, res) => {
+// Endpoint for generating a token and sending it to the backend
+app.post('/generate-and-send-token', (req, res) => {
   const { sub } = req.body;
-  console.log('Received sub:', sub); // Add this log to see the incoming data
 
   if (!sub) {
     return res.status(400).json({ error: 'Subject (sub) not provided' });
@@ -25,34 +25,20 @@ app.post('/generate-token', (req, res) => {
   };
 
   const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-  console.log('Generated token:', token); // Add this log to see the generated token
-  res.json({ token });
-});
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.header('Authorization');
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Access denied. Token not provided.' });
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  jwt.verify(token, privateKey, { algorithms: ['RS256'] }, (err, user) => {
-    if (err) return res.status(403).json({ error: 'Invalid token.' });
-
-    // Store the user information in the request object for later use
-    req.user = user;
-    next(); // Call the next middleware or route handler
-  });
-}
-
-
-
-app.get('/backend', authenticateToken, (req, res) => {
-  res.json({ message: 'Authorized access to the backend!', user: req.user });
+  // Send the generated token to the backend
+  axios
+    .post('http://127.0.0.1:4000/api/tasks', { token })
+    .then(() => {
+      console.log('Token sent to the backend successfully');
+      res.json({ message: 'Token sent to the backend successfully' });
+    })
+    .catch((error) => {
+      console.error('Failed to send token to the backend:', error.message);
+      res.status(500).json({ error: 'Failed to send token to the backend' });
+    });
 });
 
 app.listen(port, () => {
   console.log(`Authentication service is running on http://localhost:${port}`);
 });
-
