1a78637f4e8656561dfad55a675af2097c9cbe7a
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-24 15:27:11 +0530

    Update auth-service.js

--- auth-service.js
+++ auth-service.js
@@ -5,25 +5,25 @@ const jwt = require('jsonwebtoken');
 const port = 5001;
 const app = express();
 
-const secretKey = process.env.SECRET_KEY; // Replace this with your actual secret key
+const privateKey = fs.readFileSync('./private_key.pem'); // Use your environment variable here
 
 app.use(cors());
 app.use(bodyParser.json());
 
 // Endpoint for generating a token
 app.post('/generate-token', (req, res) => {
-  const { publicKey } = req.body;
-  console.log('Received publicKey:', publicKey); // Add this log to see the incoming data
+  const { sub } = req.body;
+  console.log('Received sub:', sub); // Add this log to see the incoming data
 
-  if (!publicKey) {
-    return res.status(400).json({ error: 'Public key not provided' });
+  if (!sub) {
+    return res.status(400).json({ error: 'Subject (sub) not provided' });
   }
 
   const payload = {
-    sub: publicKey,
+    sub: sub,
   };
 
-  const token = jwt.sign(payload, secretKey, { algorithm: 'HS256' });
+  const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
   console.log('Generated token:', token); // Add this log to see the generated token
   res.json({ token });
 });
@@ -31,4 +31,3 @@ app.post('/generate-token', (req, res) => {
 app.listen(port, () => {
   console.log(`Authentication service is running on http://localhost:${port}`);
 });
-
